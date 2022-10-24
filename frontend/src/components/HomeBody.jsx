470de3b1389e80e25dfef986e8ece3878562a388
Robert FR Coding <robertfrcoding@gmail.com>
2022-10-24 17:53:59 +0100

    carrusel

--- frontend/src/components/HomeBody.jsx
+++ frontend/src/components/HomeBody.jsx
@@ -3,21 +3,21 @@ export const HomeBody = () =>{
         <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <img className="d-block w-100" src="../images/17010.jpg" alt="First slide"/>
+                    <img className="d-block w-100" src="../images/blockchain.jpg" alt="First slide"/>
                     <div className="carousel-caption d-none d-md-block">
                         <h5>First slide label</h5>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block w-100" src="../images/20459156.jpg" alt="Second slide"/>
+                    <img className="d-block w-100" src="../images/map.jpg" alt="Second slide"/>
                     <div className="carousel-caption d-none d-md-block">
                         <h5>Second slide label</h5>
                         <p>Some representative placeholder content for the Second slide.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img className="d-block w-100" src="../images/WallpaperDog-20459078.jpg" alt="Third slide"/>
+                    <img className="d-block w-100" src="../images/mining.jpg" alt="Third slide"/>
                     <div className="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                         <p>Some representative placeholder content for the Third slide.</p>
