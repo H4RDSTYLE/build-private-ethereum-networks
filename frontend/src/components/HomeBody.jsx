e6706fc789bb61a458a478c9350d34341afed65d
Massimiliano <cirinomaximiliano@gmail.com>
2022-10-27 15:19:02 +0200

    página de inicio, sección de contacto

--- frontend/src/components/HomeBody.jsx
+++ frontend/src/components/HomeBody.jsx
@@ -1,3 +1,8 @@
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { FaTelegramPlane } from 'react-icons/fa';
+import { BsDiscord } from 'react-icons/bs';
+
 export function HomeBody() {
     return <div>
 
@@ -38,15 +43,10 @@ export function HomeBody() {
             <p></p>
             {/* Texto e imagenes centrales */}
             <section class="projects-section bg-light" id="projects">
-                <div class="container px-4 px-lg-5">
+                <div class="container px-4 px-lg-7">
                     <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
-                        <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="featured-text text-center text-lg-left">
-                                <h4>Shoreline</h4>
-                                <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
-                            </div>
-                        </div>
+                        <h2 class="text-dark text-center mt-1 font-semibold tracking-tight text-slate-900 text-4xl md:text-6xl">Crea tu propia Blockchain</h2>
+                        <p class="text-center text-dark-50 mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quam labore quasi nesciunt. Nihil harum, error nisi maiores saepe mollitia praesentium, architecto tenetur temporibus eos consequuntur, laudantium cupiditate quos debitis!</p>
                     </div>
                     <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
                         <div class="col-lg-6"><img class="img-fluid" src="../images/mining.jpg" alt="..." /></div>
@@ -54,8 +54,8 @@ export function HomeBody() {
                             <div class="bg-black text-center h-100 project">
                                 <div class="d-flex h-100">
                                     <div class="project-text w-100 my-auto text-center text-lg-left">
-                                        <h4 class="text-black">Misty</h4>
-                                        <p class="mb-0 text-black-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                                        <h4 class="text-dark">Rápido</h4>
+                                        <p class="mb-0 text-dark-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius delectus voluptatum quidem odio animi mollitia molestias, ab adipisci debitis iusto ratione facere ullam illum possimus architecto aspernatur unde assumenda nihil?</p>
                                         <hr class="d-none d-lg-block mb-0 ms-0" />
                                     </div>
                                 </div>
@@ -68,8 +68,8 @@ export function HomeBody() {
                             <div class="bg-black text-center h-100 project">
                                 <div class="d-flex h-100">
                                     <div class="project-text w-100 my-auto text-center text-lg-right">
-                                        <h4 class="text-black">Mountains</h4>
-                                        <p class="mb-0 text-black-50">Another example of a project with its respective description. These sections work well responsively as well, try this theme on a small screen!</p>
+                                        <h4 class="text-dark">Fácil</h4>
+                                        <p class="mb-0 text-dark-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolorem dolores expedita quidem vitae autem iste voluptas, libero et dignissimos nesciunt, sed repudiandae nemo non fuga excepturi, sapiente quis aspernatur?</p>
                                         <hr class="d-none d-lg-block mb-0 me-0" />
                                     </div>
                                 </div>
@@ -81,27 +81,36 @@ export function HomeBody() {
         </p >
 
         {/* Contacto */}
-        Contacto
-        < section class="contact-section bg-black" >
+        < section class="contact-section bg-secondary" >
+            <h2 class="text-light text-center mt-1 font-semibold tracking-tight text-slate-900 text-4xl md:text-6xl">Contáctanos</h2>
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-md-4 mb-3 mb-md-0">
                         <div class="card py-4 h-100">
                             <div class="card-body text-center">
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Address</h4>
+                                <h4 class="text-uppercase m-0">Dirección</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                                <img class="img-fluid" src="../images/mapa.png" alt="..." />
                             </div>
                         </div>
                     </div>
+
                     <div class="col-md-4 mb-3 mb-md-0">
                         <div class="card py-4 h-100">
                             <div class="card-body text-center">
                                 <i class="fas fa-envelope text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Email</h4>
+                                <h4 class="text-uppercase m-0">Contáctanos</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50"><a href="#!">hello@yourdomain.com</a></div>
+                                <div class="small text-black-50">
+
+                                    <p>+34 973 90 38 41</p>
+                                    <p>+34 590 10 07 50 </p>
+                                    <p>+34 781 70 03 92</p>
+                                    <p>exampleETH@example.com</p>
+                                    <p>exampleETH2@example.com</p>
+
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -109,9 +118,12 @@ export function HomeBody() {
                         <div class="card py-4 h-100">
                             <div class="card-body text-center">
                                 <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <h4 class="text-uppercase m-0">Saber más</h4>
                                 <hr class="my-4 mx-auto" />
-                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                                <p><AiOutlineTwitter /> Twitter</p>
+                                <p><FaTelegramPlane /> Telegram</p>
+                                <p><AiOutlineInstagram /> Instagram</p>
+                                <p><BsDiscord /> Discord</p>
                             </div>
                         </div>
                     </div>
@@ -120,9 +132,12 @@ export function HomeBody() {
                     <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                     <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                     <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                    <p>                <p></p>
+                    </p>
                 </div>
             </div>
-        </section>
-
+        </section >
     </div >
+
+
 }
