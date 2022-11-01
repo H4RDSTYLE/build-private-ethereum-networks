1b42f8aed21eb72d0da5707d0d037999cb2125f0
Massimiliano <cirinomaximiliano@gmail.com>
2022-11-01 17:19:08 +0100

    explorerh.jsx y explorer.js (chainid)

--- backend/explorer.js
+++ backend/explorer.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router()
 const Web3 = require("Web3")
 
-module.exports =  router
+module.exports = router
 
-const URL_INFURA = "https://mainnet.infura.io/v3/383484de7aee48088db56adca8badbfc"
-const web3 = new Web3(URL_INFURA)
+const culo = "http://localhost:8575"
+const web3 = new Web3(culo)
 
 router.get("/", async (req, res) => {
     const bloque = await web3.eth.getBlockNumber();
@@ -13,28 +13,37 @@ router.get("/", async (req, res) => {
 })
 
 router.get("/bloque/:bloque", async (req, res) => {
-    try{
+    try {
         const bloque = await web3.eth.getBlock(req.params.bloque)
         res.send(bloque)
-    } catch (error){
-        res.status(500).send({mensaje: error.message })
-    }    
+    } catch (error) {
+        res.status(500).send({ mensaje: error.message })
+    }
 })
 
 router.get("/tx/:tx", async (req, res) => {
-    try{
+    try {
         const tx = await web3.eth.getTransaction(req.params.tx)
         res.send(tx)
-    } catch (error){
-        res.status(500).send({mensaje: error.message })
-    }     
+    } catch (error) {
+        res.status(500).send({ mensaje: error.message })
+    }
 })
 
 router.get("/balance/:address", async (req, res) => {
-    try{
-    const balance = await web3.eth.getBalance(req.params.address)
-    res.send({ balance, ethers: balance / 1e18 , ethers2: web3.utils.fromWei(balance, 'ether')})
-} catch (error){
-    res.status(500).send({mensaje: error.message })
-}   
+    try {
+        const balance = await web3.eth.getBalance(req.params.address)
+        res.send({ balance, ethers: balance / 1e18, ethers2: web3.utils.fromWei(balance, 'ether') })
+    } catch (error) {
+        res.status(500).send({ mensaje: error.message })
+    }
+})
+
+router.get("/chainid", async (req, res) => {
+    try {
+        const chainid = await web3.eth.getChainId()
+        res.send({ chainid })
+    } catch (error) {
+        res.status(500).send({ mensaje: error.message })
+    }
 })
